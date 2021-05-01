e44ea911dc7ace50c03e9aa82f9c8dc00a6df6a3
vishal-raj15 <vishalraj1380@gmail.com>
2021-05-02 05:10:21 +0530

    first commit

--- src/components/header.js
+++ src/components/header.js
@@ -11,18 +11,20 @@ export class header extends Component {
         return (
             <div className="header">
 
-                <div className='left'> <img src={logo} style={{ width:'25px' , height:'25px' , paddingRight:'10px' , paddingTop:'10px'}} />
+                <div className='left'>
+                    
+                     <img src={logo} style={{ width:'45px' , height:'45px' , paddingRight:'10px' , paddingTop:'10px'}} />
 
                    <div style={{ float:'right' , paddingTop:'15px'}}> My page</div> 
 
                 </div>
 
-                <div style={{ position:'absolute' ,right:0 , width:'180px' , height:'50px' , top:'10px'}}>
+                <div style={{ position:'absolute' ,right:0 , width:'200px' , height:'50px' , top:'0px'}}>
 
                 <div className='middle'>
-                 <img className='im' src={me} style={{ width:'20px' , height:'20px' , paddingRight:'5px' }} />
+                 <img className='im' src={me} style={{ width:'20px' , height:'20px' }} />
 
-                    <div className='name' style={{ float:'right', fontSize:'12px'}}> Vishal</div> 
+                <div className='name' style={{ fontSize:'12px'}}> Vishal</div> 
 
                 </div>
 
---
+++ src/components/main.js
@@ -0,0 +1,34 @@
+import React, { Component } from 'react'
+
+
+export class main extends Component {
+    render() {
+        return (
+            <div className='main'>
+                
+                <div className='screen'>
+                    <div className= 'sleft'></div>
+                    <div className='sright'></div>
+                </div>
+              
+
+
+            </div>  
+        )
+    }
+}
+
+export default main
+
+
+{/* <div className="modal-body row">
+<div className="col-2"  style={{ backgroundColor:'red'}}>
+
+    heky 
+    {/* <!-- Your first column here --> */}
+// </div>
+// <div className="col-md-10"  style={{ backgroundColor:'red'}}>
+//     hedhjk
+//     {/* <!-- Your second column here --> */}
+// </div>
+// </div> */}
